5acc1b518ee1175f41dc30808c54a67c9f8b3761
urbano20894@iesmarenostrum.com <urbano20894@iesmarenostrum.com>
2022-06-29 10:00:23 +0200

    refactorizacion clase gms

--- ExamenED-2122-EX/gmsconv.cs
+++ ExamenED-2122-EX/gmsconv.cs
@@ -4,42 +4,42 @@ namespace gsmconv
 {
     public class gms
     {
-        private int g; // grados
-        private int m; // minutos
-        private double s; // segundos
-        private int maxg;  // valor límite para los grados
+        private int grados; 
+        private int minutos; 
+        private double segundos; 
+        private int maximosGrados;  
 
         public gms(int max)
         {
-            G = M = 0;
-            S = 0.0;
-            this.maxg = max;
+            Grados = Minutos = 0;
+            Segundos = 0.0;
+            this.maximosGrados = max;
         }
 
-        public int G
+        public int Grados
         {
-            get => g;
+            get => grados;
             set
             {
-                if (value >= -maxg && value <= maxg) g = value;
+                if (value >= -maximosGrados && value <= maximosGrados) grados = value;
                 else throw new ArgumentOutOfRangeException("Grados no válidos");
             }
         }
-        public int M
+        public int Minutos
         {
-            get => m;
+            get => minutos;
             set
             {
-                if (value >= 0 && value <= 60) m = value;
+                if (value >= 0 && value <= 60) minutos = value;
                 else throw new ArgumentOutOfRangeException("Minutos no válidos");
             }
         }
-        public double S
+        public double Segundos
         {
-            get => s;
+            get => segundos;
             set
             {
-                if (value >= 0 && value <= 60) s = value;
+                if (value >= 0 && value <= 60) segundos = value;
                 else throw new ArgumentOutOfRangeException("Segundos no válidos");
             }
         }
@@ -47,26 +47,26 @@ namespace gsmconv
 
     public class coord
     {
-        public gms lon = new gms(180); // longitud
-        public gms lat = new gms(90); // latitud
+        public gms longitud = new gms(180); 
+        public gms latitud = new gms(90); 
 
-        public coord(double glong, double glat)
+        public coord(double gLongitud, double gLatituditud)
         {
             // primero pasamos la longitud de grados a GMS
-            lon.G = (int)glong;
+            longitud.Grados = (int)gLongitud;
             // ahora calculamos los minutos, hay que eliminar el signo
-            glong = Math.Abs((glong - lon.G)*60.0);
-            lon.M = (int)(glong);
+            gLongitud = Math.Abs((gLongitud - lon.G)*60.0);
+            longitud.Minutos = (int)(gLongitud);
             // por último los segundos
-            lon.S = (glong - lon.M)*60.0;
+            longitud.Segundos = (gLongitud - longitud.Minutos)*60.0;
  
             // Ahora hacemos lo mismo con la latitud
-            lat.G = (int)glat;
+            latitud.Grados = (int)gLatitud;
             // ahora calculamos los minutos, hay que eliminar el signo
-            glat = Math.Abs((glat - lat.G)*60.0);
-            lat.M = (int)(glat);
+            gLatitud = Math.Abs((gLatitud - latitud.Grados)*60.0);
+            latitud.Minutos = (int)(gLatitud);
             // por último los segundos
-            lat.S = (glat - lat.M)*60.0;
+            latitud.Segundos = (gLatitud - latitud.Minutos)*60.0;
         }
     }
 }
